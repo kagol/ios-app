ff5e36d1ab208cbcad291bf3540ea600f533e6ec
kagol <kagol@sina.com>
2018-03-09 21:46:44 +0800

    add Quiz

---
+++ Quiz/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by iroot on 2018/3/9.
+//  Copyright © 2018年 kagol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by iroot on 2018/3/9.
+//  Copyright © 2018年 kagol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
