fe5985c46b50fc1421e68a9a63f0271aafc80e06
kagol <kagol@sina.com>
2018-03-20 20:48:53 +0800

    add Hypnosis

---
+++ Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by iroot on 2018/3/19.
+//  Copyright © 2018年 kagol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by iroot on 2018/3/20.
+//  Copyright © 2018年 kagol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
